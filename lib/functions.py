185558996174ee19269f38e56a8712d15c214d57
dennis <dennis.mwangi@student.moringaschool.com>
2023-08-16 15:10:48 +0300

    Completed assignment

--- lib/functions.py
+++ lib/functions.py
@@ -2,15 +2,28 @@
 
 def greet_programmer():
     pass
+    print("Hello, programmer!")
 
 def greet(name):
     pass
+    print(f"Hello, {name}!")
+
+greet("Guido")
 
 def greet_with_default(name="programmer"):
     pass
+    print(f"Hello, {name}!")
+
+greet_with_default()
 
 def add(num1, num2):
     pass
+    return(num1 + num2)
+
+add(3, 2)
+
 
 def halve(number):
     pass
+    return number / 2
+
